417a6f741506d401fc7c4747f4f27563ebfbed30
= <codymjones@ymail.com>
2014-04-03 15:54:43 -0700

    Commiting stuff

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name = "Cody Jones"
+git config --global user.email "codymjones@ymail.com"
+git config --global color.ui "auto"
+git config --global core.editor "vim"
